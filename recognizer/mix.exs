4dc3f5baa5a49b27f572ce31cdf709920afe96e3
Michał Śledź <michalsledz34@gmail.com>
2024-07-19 21:57:53 +0200

    [Recognizer] Release version 0.1.0

--- recognizer/mix.exs
+++ recognizer/mix.exs
@@ -4,7 +4,7 @@ defmodule Recognizer.MixProject do
   def project do
     [
       app: :recognizer,
-      version: "0.1.0-dev",
+      version: "0.1.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
