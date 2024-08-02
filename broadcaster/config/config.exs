a743ae09d978bec5d4d494d56aab298f4c5d6ffb
Łukasz Wala <lukaszpwala@gmail.com>
2024-08-02 10:48:55 +0200

    Turn off the logs

--- broadcaster/config/config.exs
+++ broadcaster/config/config.exs
@@ -47,7 +47,7 @@ config :tailwind,
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id],
-  level: :debug
+  level: :info
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
--- broadcaster/config/prod.exs
+++ broadcaster/config/prod.exs
@@ -9,7 +9,7 @@ config :broadcaster, BroadcasterWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :debug
+config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
