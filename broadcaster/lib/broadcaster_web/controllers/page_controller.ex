1f16fd220e69ae7253e0b6dec15d598bc766a95b
Michał Śledź <michalsledz34@gmail.com>
2024-08-30 12:38:04 +0200

    [Broadcaster] Add stream title and description (#53)

--- broadcaster/lib/broadcaster_web/controllers/page_controller.ex
+++ broadcaster/lib/broadcaster_web/controllers/page_controller.ex
@@ -2,7 +2,11 @@ defmodule BroadcasterWeb.PageController do
   use BroadcasterWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home, page_title: "Home")
+    render(conn, :home,
+      page_title: "Home",
+      title: Application.get_env(:broadcaster, :title, ""),
+      description: Application.get_env(:broadcaster, :description, "")
+    )
   end
 
   def panel(conn, _params) do
@@ -11,6 +15,14 @@ defmodule BroadcasterWeb.PageController do
 
   def delete_chat_message(conn, %{"id" => id}) do
     BroadcasterWeb.Endpoint.broadcast!("stream:chat", "delete_chat_msg", %{id: id})
+    send_resp(conn, 200, "")
+  end
+
+  def config_stream(conn, _params) do
+    {:ok, body, conn} = Plug.Conn.read_body(conn)
+    %{"title" => title, "description" => description} = Jason.decode!(body)
+    Application.put_env(:broadcaster, :title, title)
+    Application.put_env(:broadcaster, :description, description)
     send_resp(conn, 200, "")
   end
 end
