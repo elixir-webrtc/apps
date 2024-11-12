3e055c7c0f5b9c6fe03285f0b2a2c7bcdc2fe90f
Jakub Pisarek <99591440+sgfn@users.noreply.github.com>
2024-11-12 16:37:00 +0100

    [Broadcaster] Fix release overlays. Release 0.7.1

--- broadcaster/mix.exs
+++ broadcaster/mix.exs
@@ -4,7 +4,7 @@ defmodule Broadcaster.MixProject do
   def project do
     [
       app: :broadcaster,
-      version: "0.7.0",
+      version: "0.7.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
--- broadcaster/rel/overlays/bin/server.bat
+++ broadcaster/rel/overlays/bin/server.bat
@@ -1,2 +1,7 @@
+if "%DISTRIBUTION_MODE%"=="k8s" (
+  set RELEASE_DISTRIBUTION=name
+  set RELEASE_NODE="broadcaster@%POD_IP%"
+)
+
 set PHX_SERVER=true
 call "%~dp0\broadcaster" start
