0258dae4e98dea5d8a624f183588bc629d31739d
Michał Śledź <michalsledz34@gmail.com>
2024-12-06 13:00:58 +0100

    Start dist config before starting Forwarder

--- broadcaster/lib/broadcaster/application.ex
+++ broadcaster/lib/broadcaster/application.ex
@@ -12,24 +12,31 @@ defmodule Broadcaster.Application do
 
   @impl true
   def start(_type, _args) do
+    dist_config =
+      case Application.fetch_env!(:broadcaster, :dist_config) do
+        nil ->
+          []
+
+        config ->
+          [{Cluster.Supervisor, [[cluster: config], [name: Broadcaster.ClusterSupervisor]]}]
+      end
+
+    # Start dist_config before starting Forwarder, 
+    # as Forwarder asks other nodes for their inputs.
     children =
       [
         BroadcasterWeb.Telemetry,
         {Phoenix.PubSub, name: Broadcaster.PubSub},
         BroadcasterWeb.Endpoint,
-        BroadcasterWeb.Presence,
-        Broadcaster.PeerSupervisor,
-        Broadcaster.Forwarder,
-        Broadcaster.ChatHistory,
-        {Registry, name: Broadcaster.ChatNicknamesRegistry, keys: :unique}
+        BroadcasterWeb.Presence
       ] ++
-        case Application.fetch_env!(:broadcaster, :dist_config) do
-          nil ->
-            []
-
-          config ->
-            [{Cluster.Supervisor, [[cluster: config], [name: Broadcaster.ClusterSupervisor]]}]
-        end
+        dist_config ++
+        [
+          Broadcaster.PeerSupervisor,
+          Broadcaster.Forwarder,
+          Broadcaster.ChatHistory,
+          {Registry, name: Broadcaster.ChatNicknamesRegistry, keys: :unique}
+        ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
